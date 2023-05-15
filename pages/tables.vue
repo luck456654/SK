98a1e7f5af0567672e4e315003086a6d2d3a10e6
luck456654 <44877772+luck456654@users.noreply.github.com>
2023-05-15 16:34:17 +0300

    Update tables.vue

--- pages/tables.vue
+++ pages/tables.vue
@@ -1,25 +1,25 @@
 <template>
   <div class="tables">
-    <div class="wrapper">
+    <div class="wrapper" v-if="getell.length">
       <div v-for="(item, index) in user"
-                :key="index" class="mark" @click="changeMark" :id="item">        
+                :key="index" :class = "(item == activeMark) ? 'mark active': 'mark'" @click="changeMark" :id="item">
                {{ item }}
      </div>
      <div v-for="(item, index) in arr"
-                :key="index" class="mark" @click="changeMark" :id="item">        
+                :key="index" :class = "(item == activeMark) ? 'mark active': 'mark'" @click="changeMark" :id="item">
       {{ item }}
      </div>
   </div>
-   
-  <table class="table">
+
+  <table class="table" v-if="getell.length">
     <tbody>
             <tr class="dnone"><th colspan="5" style="background-color: #bdd0d0!important;">Резерв неоплаченных остатков держится 14 календарных дней</th></tr>
             <tr class="dnone" style="background-color: #bdd0d0!important;"><th>Чертежи необходимо обязательно согласовать с Пашей</th><th colspan="4">проведена инвентаризация 25.01.2023г</th></tr>
             <tr style="background-color: #bdd0d0!important;"><th>Пистолет записывается с нижнего угла по часовой стрелке</th><th>Менеджер</th><th>Дата</th><th>Статус</th><th>Комментарий</th><th>Оттенок</th><th>Удалить</th></tr>
-            <tr style="background-color: #bdd0d0!important;"><th>Цвет</th><th>Размер</th><th colspan="3">Внутр. обозначение</th><th></th><th></th></tr>
-          
-             <tr v-for="(item, index) in getell"  
-                  :key="index" :class = "(index % 2 != 0)?'bgcolor':'bgc'">
+            <tr style="background-color: #bdd0d0!important;"><th>Цвет</th><th>Размер</th><th class="wide-3l">Внутр. обозначение</th><th></th><th></th></tr>
+
+             <tr v-for="(item, index) in getell"
+                  :key="index" :class = "[(index % 2 != 0)?'bgcolor':'bgc', (item.id != activeMark)? 'dnone': '']">
                   <td class="value wrapp"   v-if="item.id==activeMark"><input :placeholder="item.name" v-model="item.name" @input = "edit(item.idx,'name')" class="input inputs"/></td>
                   <td class="value"  v-if="item.id==activeMark"><input :placeholder="item.nameMen" v-model="item.nameMen" @input = "edit(item.idx,'nameMen')" class="input inputs"/></td>
                   <td class="value"  v-if="item.id==activeMark"><input :placeholder="item.data" v-model="item.data" @input = "edit(item.idx,'data')" class="input inputs"/></td>
@@ -34,22 +34,22 @@
 
   </table>
   <div class="wrappbtn">
-  <input type="button" class="addbtn" value="+" @click="addElls"/>
+  <input type="button" class="addbtn" v-if="getell.length" value="+" @click="addElls"/>
   </div>
   <p><input placeholder="Наименование вкладки"  v-model="nameMark" class="input"/>
   <input type="button" value="Добавить вкладку" class="btn" @click="addMark">
-  </p> 
-  
-  
+  </p>
+
+
 </div>
 </template>
-  
+
   <script>
   export default {
   name: "Table",
   data() {
     return {
-     activeMark:"Акрил", 
+     activeMark:"Акрил",
      nameMark:"",
      name:"",
      nameMen:"",
@@ -70,7 +70,7 @@
   this.$nextTick(function () {
     this.getdata()
     console.log(this.$route)
-    
+
   })
   },
   methods: {
@@ -108,10 +108,10 @@
     async getdata(ins){
       const user = await this.$axios
           .$post("https://стильный-камень.рф/api/components/saveTable.php",{
-            action: 'getting',            
+            action: 'getting',
           })
           .then((data) => {
-            
+
             console.log(data);
             this.getell=data;
             if(!ins){
@@ -127,19 +127,35 @@
     },
     addMark(){
       this.arr.push(this.nameMark)
-      },
+      this.addElls2()
+    },
     gettabs(){
       for (let i=0; i<this.user.length; i++) {
         this.temp.push(this.user[i].id)
       }
       this.user=this.temp
       this.user = new Set(this.user);
-      this.user = this.user 
+      this.user = this.user
+    },
+    async addElls2(){
+      const data = await this.$axios
+        .$post("https://стильный-камень.рф/api/components/saveTable.php", {
+            action: 'add',
+            id:this.nameMark,
+            })
+          .then((data) => {
+             this.temp = data;
+             this.getdata('insert');
+             })
+          .catch((err) => {
+            console.log(err);
+          });
+        
     },
      async addElls(){
       const data = await this.$axios
-        .$post("https://стильный-камень.рф/api/components/saveTable.php", {            
-            action: 'add', 
+        .$post("https://стильный-камень.рф/api/components/saveTable.php", {
+            action: 'add',
             id:this.activeMark,
             })
           .then((data) => {
@@ -148,8 +164,9 @@
              })
           .catch((err) => {
             console.log(err);
-          });   
-    }, 
+          });
+        
+    },
     changeMark(ev){
       this.activeMark=ev.target.id
     }
@@ -158,8 +175,9 @@
   </script>
   <style scoped>
   .table{
-    border: 1px solid black;
-    border-collapse: collapse; 
+    border-collapse: collapse;
+    display: table;
+    table-layout: fixed;
   }
   .input{
     border: 1px solid black;
@@ -169,7 +187,7 @@
   .btn{
     background-color: #c8601b;
     color: white;
-  }  
+  }
   .wrapper{
     display: flex;
     flex-direction: row;
@@ -190,15 +208,16 @@
     margin-bottom: 10px;
     padding: 5px 10px;
     font-family: Arial;
-    font-size: 1vw;
-  } 
-  .mark:hover{
+    font-size: 14px;
+  }
+  .mark:hover,
+  .mark.active{
   background: #c8601b;
   color: #fff ;
   }
   .value{
   font-weight: 400;
-  } 
+  }
   .wrapp{
   display: flex;
   flex-direction: row;
@@ -215,37 +234,80 @@
     background-color: #c8601b;
   }
   .inputs{
-  background-color: unset;
+    background-color: unset;
     border: none;
     outline: none;
     color: black;
     font-size: 18px;
   }
   tbody{
-  background-color: #BDD0D0;
+    background-color: #BDD0D0;
+    flex-wrap: wrap;
+    max-width: 100%;
+    display: flex;
+    table-layout: fixed;
+    box-sizing: border-box;
+    table-layout: fixed;
+    /*border: 1px solid black;*/
+    box-shadow: 0 0px 12px rgb(0 0 0 / 99%);
   }
   th{
     padding: 10px;
-    font-size: 20px; 
+    font-size: 20px;
+    padding: 5px;
+    font-size: 13px;
   }
   tr:hover{
   background-color: #E3AF8D;
-  } 
+  }
   .bgcolor{
   background-color: #FFFFFF;
   }
   .bgc{
   background-color: #EEEEEE;
-  }  
+  }
   .dnone{
-    display: none;
-  } 
+    display: none!important;
+  }
   .left-menu {
     display: none;
   }
   .tables{
-    margin-left: -51%;
-    padding: 5vw;
+    /*margin-left: -51%;
+    padding: 5vw;*/
+    margin-left: 0;
+    padding: 15px;
+    width: 100%;
+    max-width: 100%;
+  }
+  .tables tr{
+    width: 100%;
+    max-width: 100%;
+    border: 1px solid black;
+    border-bottom: none;
+    display: flex;
+  }
+  .tables tr:last-child{
+    border-bottom: 1px solid black;
+  }
+  .tables tr td,
+  .tables th{
+    width: 14.28%;
+    max-width: 14.28%;
+    border-top: none;
+    border-bottom: none;
+    border-left: none;
+    border-right: 1px solid black;
+  }
+  .tables th.wide-3l{
+    width: 42.84%;
+    max-width: 42.84%;
+  }
+  .tables tr td:last-child,
+  .tables th:last-child,
+  .tables .not-right-line{
+    border-bottom: none;
+    border-right: none;
   }
   .dellicon{
     display: flex;
@@ -267,18 +329,42 @@
   }
   .addbtn:hover,.delbtn:hover{
     background-color: #c8601b;
-    color: white; 
+    color: white;
   }
   .inputlinck{
-    margin-left: 9px;
-    padding: 10px;
-    font-size: 20px;
+    margin-left: 0;
+    padding: 5px;
+    font-size: 12px;
     margin-right: 12px;
-    margin-bottom: 10px;
-    margin-top: 10px;
+    margin-bottom: 0;
+    margin-top: 0;
+    max-width: 100%;
   }
   .imgott{
-    width: 287px;
-    height: 54px;
+    /*width: 287px;
+    height: 54px;**/
+    width: 100%;
+    height: auto;
   }
-  </style>
+  td .btninput{
+    margin: auto;
+  }
+@media(max-width: 500px){
+    .tables tr td,
+    .tables th {
+    overflow: hidden;
+    font-size: 9px;
+    word-wrap: break-word;
+  }
+  .inputs{
+    overflow: hidden;
+    font-size: 10px;
+    word-wrap: break-word;
+  }
+  .input {
+    height: unset;
+    padding: 0;
+    word-wrap: break-word;
+  }
+}
+</style>
